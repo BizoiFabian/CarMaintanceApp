fd060e0fa5ee4a9df39f57730bb3369bc716a62c
Fabian <fabianbizoi50@gmail.com>
2025-02-13 17:44:37 +0200

    Init ModelsForMaintance

---
+++ CarMaintanceApp/Models/CarMaintenance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarMaintanceApp.Models
+{
+    public class CarMaintenance
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string CarName { get; set; }  // Numele mașinii
+
+        [Required]
+        public string MaintenanceType { get; set; }  // Tipul intervenției (ex: schimb ulei, filtre etc.)
+
+        public DateTime MaintenanceDate { get; set; }  // Data întreținerii
+
+        public int MileageAtService { get; set; }  // Kilometrajul la momentul întreținerii
+
+        public int NextServiceMileage { get; set; }  // Kilometraj estimat pentru următoarea întreținere
+    }
+}
